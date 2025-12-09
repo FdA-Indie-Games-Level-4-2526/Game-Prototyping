305a8713462aa58c7083ac9e31557d9706da13c4
Cameron Glover <glovercameron71@gmail.com>
2025-12-09 14:30:34 +0000

    Main Push

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -18,6 +18,11 @@ public class SceneController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (fadingScript == null)
+        {
+            fadingScript = GetComponent<FadingScript>();
+        }
     }
 
     public void NextLevel()
